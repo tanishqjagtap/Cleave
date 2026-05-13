1d8a5c449c59627aa0e5ed1c7c7fa7b10573c19f
tanishqjagtap <tanishqjagtap523@gmail.com>
2026-05-13 21:23:39 +0530

    DAY2 : Working on player movement

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -3,19 +3,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset;
-    public float smoothSpeed = 5f;
+    public Vector3 offset = new Vector3(0, 5, -7);
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = player.position + offset;
-
-        Vector3 smoothPosition = Vector3.Lerp(
-            transform.position,
-            desiredPosition,
-            smoothSpeed * Time.deltaTime
-        );
-
-        transform.position = smoothPosition;
+        transform.position = player.position + offset;
     }
 }
