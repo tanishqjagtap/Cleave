6a3075ff2091fab63387e983732dc944c39738fa
tanishqjagtap <tanishqjagtap523@gmail.com>
2026-06-02 16:02:35 +0530

    Working on navigation

--- Assets/Scripts/UI/MainScene/PauseMenu.cs
+++ Assets/Scripts/UI/MainScene/PauseMenu.cs
@@ -1,33 +1,74 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    [Header("Panels")]
     public GameObject pauseMenu;
+    public GameObject settingsPanel;
 
-    bool isPaused = false;
+    private bool isPaused = false;
 
-    void Update()
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+        settingsPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isPaused)
+            // If settings is open, go back to pause menu
+            if (settingsPanel.activeSelf)
+            {
+                CloseSettings();
+                return;
+            }
+
+            if (pauseMenu.activeSelf)
                 Resume();
             else
                 Pause();
         }
     }
 
-    void Pause()
+    public void Pause()
     {
         pauseMenu.SetActive(true);
+
         Time.timeScale = 0f;
         isPaused = true;
     }
 
     public void Resume()
     {
+        settingsPanel.SetActive(false);
         pauseMenu.SetActive(false);
+
         Time.timeScale = 1f;
         isPaused = false;
+    }
+
+    public void OpenSettings()
+    {
+        // DO NOT disable pauseMenu
+        // this was causing your weird issues
+
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Intro Menu");
     }
 }
